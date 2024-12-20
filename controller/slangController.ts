8f32123a449900d27e4b6a776cd5fb1e58b883a5
WizzyCodes <wisdomiberi832@gmail.com>
2024-12-20 17:29:49 +0100

    slang api

--- controller/slangController.ts
+++ controller/slangController.ts
@@ -40,3 +40,46 @@ export const createSlang = async (
     });
   }
 };
+
+export const readAllSlang = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const slang = await slangModel.find().sort({
+      createdAt: -1,
+    });
+    return res.status(200).json({
+      message: "all slangs read successfully",
+      data: slang,
+      status: 200,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      message: "Error getting all slangs",
+      data: error,
+    });
+  }
+};
+
+export const readOneSlang = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { slangID } = req.params;
+
+    const readSlang = await slangModel.findById(slangID);
+
+    return res.status(200).json({
+      message: "Slang read successfully",
+      data: readSlang,
+      status: 200,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      message: "Error getting one slang",
+      data: error,
+    });
+  }
+};
--- router/slangRouter.ts
+++ router/slangRouter.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { createSlang } from "../controller/slangController";
+import {
+  createSlang,
+  readAllSlang,
+  readOneSlang,
+} from "../controller/slangController";
 
 const slangRouter: any = Router();
 
 slangRouter.route("/create-slang/:userID").post(createSlang);
+slangRouter.route("/read-all-slang").get(readAllSlang);
+slangRouter.route("/read-one-slang/:slangID").get(readOneSlang);
 
 export default slangRouter;
