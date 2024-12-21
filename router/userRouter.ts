3969d9cca7e3accedd7deb3cb8a9349dfcbcacf9
WizzyCodes <wisdomiberi832@gmail.com>
2024-12-21 23:29:26 +0100

    updated

--- router/userRouter.ts
+++ router/userRouter.ts
@@ -5,10 +5,10 @@ import {
   readAllUsers,
   readoneUser,
 } from "../controller/userController";
-
+import { upload } from "../utils/multer";
 const router: any = Router();
 
-router.route("/create-user").post(createUser);
+router.route("/create-user").post(upload, createUser);
 router.route("/login").post(loginUser);
 router.route("/read-one-user/:userID").get(readoneUser);
 router.route("/read-all-users").get(readAllUsers);
--- utils/multer.ts
+++ utils/multer.ts
@@ -20,4 +20,4 @@ const storage = multer.diskStorage({
   },
 });
 
-export const upload = multer({ storage: storage }).single("image");
+export const upload = multer({ storage: storage }).single("avatar");
